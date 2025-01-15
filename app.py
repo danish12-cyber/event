59306f4f41c73a14262b96ea4b94ca0691a9298c
danish12-cyber <sdanish1472@gmail.com>
2025-01-15 12:43:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ st.title("Event Extractor")
 result=[]
 Url = st.text_input("Enter the Url", value="", max_chars=100)
 
-age = st.slider("Select Range of Sentence", 0, 100, 2)
+size = st.slider("Select Range of Sentence", 0, 100, 2)
 
 if st.button("Extract Event"):
-    result= pl.event_extract(Url, 2)
+    result= pl.event_extract(Url, size)
     for x in result:
         st.write(x)
      
